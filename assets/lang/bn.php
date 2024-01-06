3d0c58da035ffc526671639ee157125d1056617e
Lelin Chakma <lelinchakma1234@gmail.com>
2024-01-07 00:57:07 +0600

    ete

---
+++ assets/lang/bn.php
@@ -0,0 +1,136 @@
+<?php
+$language['menu'] = "মেনু";
+$language['dashboard'] = "ড্যাসবোর্ড";
+
+$language['personal-kit'] = "ব্যক্তিগত মেনু";
+$language['events'] = "ঘটনা";
+$language['notes'] = "চিরকুট";
+$language['routine'] = "রুটিন";
+$language['class-routine'] = "ক্লাস রুটিন";
+$language['exam-routine'] = "পরীক্ষা রুটিন";
+$language['daily-routine'] = "দৈনিক রুটিন";
+$language['chat'] = "আলাপ আলোচনা";
+$language['modules'] = "মডিউল";
+$language['classroom'] = "শ্রেনিকক্ষ";
+
+$language['study-pages'] = "পড়ালেখা";
+$language['quiz-page'] = "কুইজ";
+$language['course-page'] = "কোর্স";
+$language['tutorial-page'] = "টিউটোরিয়াল";
+$language['community-page'] = "পড়ালেখার গ্রুপ";
+$language['book-shop'] = "বইয়ের দোকান";
+$language['world-history'] = "বিশ্ব ইতিহাস";
+
+$language['profile'] = "প্রোফাইল";
+$language['courses'] = "কোর্সগুলি";
+$language['log-out'] = "প্রস্থান করা";
+
+
+// Form Placeholder
+$language['search-for-results'] = "ফলাফল অনুসন্ধান করুন...";
+$language['search'] = "অনুসন্ধান....";
+
+
+
+//Common Word
+$language['pages'] = "পেইজ";
+$language['edit'] = "সম্পাদনা";
+$language['delete'] = "মুছে ফেলা";
+$language['download'] = "ডাউনলোড";
+$language['read-more'] = "আরো দেখুন...";
+$language['view-all'] = "সব ক্লাস দেখুন";
+$language['view-class'] = "ক্লাসটি দেখুন";
+$language['submit'] = "সাবমিট";
+
+
+
+//Index File
+$language['daily-activity'] = "দৈনিক কাজকর্ম";
+$language['recent-modules'] = "সাম্প্রতিক মডিউল";
+$language['recent-notes'] = "সাম্প্রতিক নোট";
+$language['recent-classroom']="সাম্প্রতিক ক্লাসরুম";
+$language['no-events-today']="আজকে কোনো ইভেন্ট নেই";
+
+
+
+
+//Add Button
+$language['add-module'] ="মডিউল যোগ করুন";
+$language['add-notes'] ="নোট যোগ করুন";
+$language['add-events'] ="ইভেন্ট যোগ করুন";
+
+
+
+
+//Exam Routine Section
+$language['mid-term-exam'] = "মধ্য মেয়াদী পরীক্ষা";
+$language['final-term-exam'] = "চূড়ান্ত পরীক্ষা";
+$language['no-routine-here'] = "এখানে কোনো রুটিন নেই";
+$language['add-routine'] = "রুটিন যোগ করুন";
+
+
+
+//module section 
+$language['no-module-here'] = "আপনার কোনো মডিউল নেই";
+
+
+
+// note section
+$language['no-note-here'] = "আপনার কোনো নোট নেই";
+
+
+
+//Page Classroom
+$language['stuport-classroom'] = "স্টুপোর্ট শ্রেনিকক্ষ";
+$language['classroom'] = "শ্রেনিকক্ষ";
+$language['good-morning'] = "শুভ সকাল";
+$language['stu-id'] = "ছাত্র আইডি";
+$language['no-of-class'] = "ক্লাসের সংখ্যা";
+$language['no-of-post'] = " পোস্টের সংখ্যা"; 
+$language['no-of-comments'] = "মন্তব্যের সংখ্যা";
+$language['join-class'] = "ক্লাস যোগ করুন";
+$language['create-class'] = "ক্লাস তৈরি করুন";
+$language['create-or-join'] = "ক্লাস তৈরি/যোগ করুন";
+
+
+
+//Classroom About
+$language['change-cover'] = "কভার পরিবর্তন করুন";
+$language['all-post'] = "সকল কার্যক্রম ";
+$language['people'] = "লোকজন";
+$language['images'] = "ছবিসমূহ";
+$language['link'] = "লিংক গুলো";
+$language['files'] = "ফাইল";
+$language['announce-something-to-your-class'] = "আপনার ক্লাসে কিছু ঘোষণা করুন";
+$language['post'] = "পোষ্ট";
+$language['edit-post'] = "পোস্ট সম্পাদনা করুন";
+$language['delete-post'] = "পোষ্ট মুছে ফেলুন";
+$language['group-chat'] = "গ্রুপ আলোচনা";
+$language['sms-notice'] = "SMS বার্তা";
+$language['email-notice'] = "ই-মেইল বার্তা";
+
+
+
+
+// Tutorial Part for Bangali Language//
+$language['primary'] = "প্রাথমিক ";
+$language['high'] = "মাধ্যমিক ";
+$language['college'] = "কলেজ";
+$language['university'] = "বিশ্ববিদ্যালয় ";
+$language['others'] = "অন্যান্য";
+$language['all-book-in-class-three'] = "তৃতীয় শ্রেনির সকল বই";
+$language['all-book-in-class-four'] = "চতুর্থ শ্রেনির সকল বই";
+$language['all-book-in-class-five'] = "পঞ্চম শ্রেনির সকল বই";
+$language['my-bangla-book'] = "আমার বাংলা বই";
+$language['general-math'] = "সাধারন গনিত";
+$language['general-science'] = "সাধারন বিজ্ঞান";
+$language['bagladesh-global-studies'] = " বাংলাদেশ ও বিশ্বপরিচয় ";
+$language['islam-moral-studies'] = "ইসলাম ও নৈতিক শিক্ষা";
+$language['hindu-moral-studies'] = "হিন্দুধর্ম ও নৈতিক শিক্ষা ";
+$language['buddha-moral-studies'] = "বৌদ্ধধর্ম ও নৈতিক শিক্ষা ";
+$language['christan-moral-studies'] = "খ্রিস্টধর্ম ও নৈতিক শিক্ষা";
+$language['university'] = "মেনু";
+$language['university'] = "মেনু";
+$language['university'] = "মেনু";
+
+?>
---
+++ assets/lang/en.php
@@ -0,0 +1,163 @@
+<?php 
+//Sidebar Section
+$language['menu'] = "Menu";
+$language['dashboard'] = "Dashboard";
+
+$language['personal-kit'] = "PERSONAL KIT";
+$language['events'] = "Events";
+$language['notes'] = "Notes";
+$language['routine'] = "Routine";
+$language['class-routine'] = "Class Routine";
+$language['exam-routine'] = "Exam Routine";
+$language['daily-routine'] = "Daily Routine";
+$language['chat'] = "Chat";
+$language['modules'] = "Modules";
+$language['classroom'] = "Class Room";
+
+$language['study-pages'] = "Study Pages";
+$language['quiz-page'] = "Quiz Page";
+$language['course-page'] = "Course Page";
+$language['tutorial-page'] = "Tutorial Page";
+$language['community-page'] = "Community Page";
+$language['book-shop'] = "Book Shop";
+$language['world-history'] = "World History";
+
+$language['profile'] = "Profile";
+$language['courses'] = "Courses";
+$language['log-out'] = "Log Out";
+
+
+// Form Placeholder
+$language['search-for-results'] = "Search for results...";
+$language['search'] = "Search....";
+
+
+
+//Common Word
+$language['pages'] = "Pages";
+$language['edit'] = "Edit";
+$language['delete'] = "Delete";
+$language['download'] = "Download";
+$language['read-more'] = "Read More...";
+$language['view-all'] = "View All Class";
+$language['view-class'] = "View Class";
+$language['submit'] = "Submit";
+
+
+
+//Index File
+$language['daily-activity'] = "Daily Activity";
+$language['exam-routine'] = "Examination Routine";
+$language['recent-modules'] = "Recent Modules";
+$language['recent-notes'] = "Recent Notes";
+$language['recent-classroom'] = "Recent Classroom";
+$language['no-events-today'] = "No Events Today";
+
+
+
+
+//Add Button
+$language['add-module'] ="Add Module";
+$language['add-notes'] ="Add Notes";
+$language['add-events'] ="Add Events";
+
+
+
+
+//module sesction
+$language['no-module-here'] = "No Module Here";
+
+
+
+// note section
+$language['no-note-here'] = "No Note Here";
+
+
+
+//daily routine section
+$language['daily_routine'] = "Daily Routine";
+$language['friday'] = "Friday";
+$language['saturday'] = "Saturday";
+$language['sunday'] = "Sunday";
+$language['monday'] = "Monday";
+$language['tuesday'] = "Tuesday";
+$language['wednessday'] = "Wednessday";
+$language['thursday'] = "Thursday";
+$language['today'] = "Today";
+$language['tomorrow'] = "Tomorrow";
+$language['complete'] = "Complete";
+$language['cencel'] = "Cencel";
+$language['time'] = "Time";
+$language['new'] = "New";
+$language['rejected'] = "Rejected";
+$language['completed'] = "Completed";
+$language['add_task'] = "Add Task";
+
+
+
+
+//Exam Routine Section
+$language['no-routine-here'] = "No Routine Here";
+$language['add-routine'] = "Add Routine";
+$language['mid-term-exam'] = "Mid Term Exam";
+$language['final-term-exam'] = "Final Exam";
+
+
+
+
+//Page Classroom
+$language['stuport-classroom'] = "StuPort Classroom";
+$language['classroom'] = "Classroom";
+$language['good-morning'] = "Good Morning";
+$language['stu-id'] = "Student ID";
+$language['no-of-class'] = "Number of Class";
+$language['no-of-post'] = "Number of Post";
+$language['no-of-comments'] = "Number of Comments";
+$language['join-class'] = "Join Class";
+$language['create-class'] = "Create Class";
+$language['create-or-join'] = "Create/Join Class";
+
+
+
+
+//Classroom About
+$language['change-cover'] = "Change Cover";
+$language['all-post'] = "All Post ";
+$language['people'] = "People";
+$language['images'] = "Images";
+$language['link'] = "Link";
+$language['files'] = "Files";
+$language['announce-something-to-your-class'] = "Announce something to your class";
+$language['post'] = "Post";
+$language['edit-post'] = "Edit Post";
+$language['delete-post'] = "Delete Post";
+$language['group-chat'] = "Group Chat";
+$language['sms-notice'] = "SMS Notice";
+$language['email-notice'] = "EMail Notice";
+
+
+
+
+
+// Tutorial Part for Bangali Language//
+$language['primary'] = "Primary ";
+$language['high'] = "High ";
+$language['college'] = "College";
+$language['university'] = "University ";
+$language['others'] = "Others";
+$language['all-book-in-class-three'] = "All Book in Class Three";
+$language['all-book-in-class-four'] = "All Book in Class Four";
+$language['all-book-in-class-five'] = "All Book in Class Five";
+$language['my-bangla-book'] = "My Bangla Book";
+$language['general-math'] = "General Mathemathics";
+$language['general-science'] = "General Science";
+$language['bagladesh-global-studies'] = " Bangladesh and Global Studies ";
+$language['islam-moral-studies'] = "Islam and Moral Studies";
+$language['hindu-moral-studies'] = "Hindu and Moral Studies ";
+$language['buddha-moral-studies'] = "Buddha and Moral Studies ";
+$language['christan-moral-studies'] = "Christan and Moral Studies";
+$language['university'] = "মেনু";
+$language['university'] = "মেনু";
+$language['university'] = "মেনু";
+
+?>
